fc196cef89f0b5de965919c6beb9c3680ec03128
Prince Singh <marvelousprince012233@gmail.com>
2021-12-16 00:25:21 +0530

    Updated for csab 2021

--- ORCR/json_to_excel.js
+++ ORCR/json_to_excel.js
@@ -1,8 +1,8 @@
 const xlsx = require('xlsx');
-const table_json = require('./source/josaa2021.json');
+const table_json = require('./source/csab2021.json');
 
-const file = xlsx.readFile('./output/ORCR_josaa.xlsx');
+const file = xlsx.readFile('./output/ORCR_csab.xlsx');
 const table_ws = xlsx.utils.json_to_sheet(table_json);
 xlsx.utils.book_append_sheet(file, table_ws);
 
-xlsx.writeFile(file, './output/ORCR_josaa.xlsx');
+xlsx.writeFile(file, './output/ORCR_csab.xlsx');
